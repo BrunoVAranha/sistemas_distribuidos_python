0ab2ea087a2583dc13548b470210b3d209abcb4a
Bruno <bruno.va.github@gmail.com>
2023-08-03 13:20:47 -0300

    final

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import json
 import random
 import socket
+import time
 
 from mensagem import Mensagem
 
@@ -10,45 +11,68 @@ ip = "127.0.0.1"
 porta = []
 client_sockets = []
 
+# dicionario que armazena Chave-valor (key e timestamp)
+keyDict = {"1": "10"}
+
 menu_input = input("digite INIT para criar o cliente: ")
 if menu_input == "INIT":
-
+    # Se conectar aos três servidores
     for i in range(3):
-        print('a')
         porta_input = int(input(f"Porta do servidor {i + 1}: "))
         porta.append(porta_input)
-
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect(('127.0.0.1', porta_input))
         client_sockets.append(client_socket)
 
-while True:
-    menu_input = input("digite PUT para inserir uma chave-valor no sistema, ou GET para receber uma chave-valor: ")
-    if menu_input == "PUT":
+    while True:
+        menu_input = input("digite PUT para inserir uma chave-valor no sistema, ou GET para receber uma chave-valor: ")
         # Gerar numero aleatorio entre 0 e 2 para escolher entre os 3 servidores
         random_number = random.randint(0, 2)
-        client_sockets[random_number].send("PUT".encode())
-
-        key_input = input("key: ")
-        value_input = input("value: ")
-
-        # Criação da mensagem
-        mensagem = Mensagem(key_input, value_input)
-
-        # Transformar a mensagem em json para trafegar no socket
-        mensagem_json = json.dumps({"key": mensagem.key, "value": mensagem.value})
-
-        # Enviar a mensagem para o servidor sorteado
-        client_sockets[random_number].send(mensagem_json.encode('utf-8'))
-
-        # Receber o PUT_OK e timestamp
-        data_timestamp = client_sockets[random_number].recv(1024).decode()
-        print(
-            f"PUT_OK key: {key_input} "
-            f"value {value_input} "
-            f"timestamp {data_timestamp} "
-            f"realizada no servidor[{ip}:{porta[random_number]}]")
-
-    elif menu_input == "GET":
-        print("get")
-
+        if menu_input == "PUT":
+            client_sockets[random_number].send("PUT".encode())
+            key_input = input("key: ")
+            value_input = input("value: ")
+            # Criação da mensagem
+            mensagem = Mensagem(key_input, value_input)
+            # Transformar a mensagem em json para trafegar no socket
+            mensagem_json = json.dumps({"key": mensagem.key, "value": mensagem.value})
+            # Enviar a mensagem para o servidor sorteado
+            client_sockets[random_number].send(mensagem_json.encode('utf-8'))
+            # Receber o PUT_OK e timestamp
+            data_timestamp = client_sockets[random_number].recv(1024).decode()
+            print(
+                f"PUT_OK key: {key_input} "
+                f"value {value_input} "
+                f"timestamp {data_timestamp} "
+                f"realizada no servidor[{ip}:{porta[random_number]}]")
+            keyDict[key_input] = data_timestamp
+        elif menu_input == "GET":
+            client_sockets[random_number].send("GET".encode())
+            key_input = input("chave para procurar: ")
+            client_sockets[random_number].send(key_input.encode())
+            # Receber a mensagem (json) do servidor
+            data = client_sockets[random_number].recv(1024).decode('utf-8')
+            if data != "NULL":
+                # deserializar json e montar tabela chave-valor
+                mensagem_dict = json.loads(data)
+                key = mensagem_dict["key"]
+                value = mensagem_dict["value"]
+                timestamp_key = mensagem_dict["timestamp"]
+                server_timestamp = client_sockets[random_number].recv(1024).decode()
+                # se a chave não existir no catalogo do cliente
+                if key not in keyDict:
+                    keyDict[key] = 0
+                    if float(timestamp_key) >= float(keyDict[key]):
+                        print(
+                            f"GET key: {key} value: {value} obtido do servidor [{ip}:{porta[random_number]}], "
+                            f"meu timestamp [{keyDict[key]}] e do servidor [{server_timestamp}]")
+                        # atualizar timestamp do cliente
+                        keyDict[key] = timestamp_key
+                elif float(timestamp_key) >= float(keyDict[key]):
+                    print(
+                        f"GET key: {key} value: {value} obtido do servidor [{ip}:{porta[random_number]}], meu timestamp ["
+                        f"{keyDict[key]}] e do servidor [{server_timestamp}]")
+                else:
+                    print("TRY_OTHER_SERVER_OR_LATER")
+            else:
+                print("NULL")
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from mensagem import Mensagem
 # IP local padrão
 ip = "127.0.0.1"
 # dicionario que armazena key-value
-key_value = {}
+key_value = {"1": ("1", "5")}
 # porta do lider
 porta_lider = 0
 # porta deste servidor
@@ -27,17 +27,10 @@ lider_connect_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lider = False
 
 
-# função que adiciona um key-value ao dicionario
-def add_key_value(key, value):
-    timestamp = time.time()
-    key_value[key] = (value, timestamp)
-
-
 def handle_client(client_socket, client_address, lider_connect_socket):
-    global key
     while True:
-        data = client_socket.recv(1024).decode('utf-8')
-        if data == "PUT":
+        operacao = client_socket.recv(1024).decode('utf-8')
+        if operacao == "PUT":
             timestamp = time.time()
             if lider:
                 # Receive JSON data from the server
@@ -47,7 +40,6 @@ def handle_client(client_socket, client_address, lider_connect_socket):
                 key = mensagem_dict["key"]
                 value = mensagem_dict["value"]
                 key_value[mensagem_dict["key"]] = (mensagem_dict["value"], timestamp)
-                print(key_value)
                 print(f"Cliente {ip}:{client_address[1]} PUT key:{key} value:{value}")
                 # replicar a chave-valor nos servidores conectados ao lider
                 mensagem = Mensagem(mensagem_dict["key"], mensagem_dict["value"], timestamp)
@@ -58,49 +50,74 @@ def handle_client(client_socket, client_address, lider_connect_socket):
                 for i in range(len(replication_socket_list)):
                     replication_socket_list[i].send("REPLICATION".encode('utf-8'))
                     replication_socket_list[i].send(mensagem_replication.encode('utf-8'))
-                    print(f"Encaminhando PUT key:{key} value: {value}.")
                     # receber a confirmação da replicação
                     replication_socket_list[i].recv(1024).decode('utf-8')
 
                 # devolver para o cliente o PUT_OK com timestamp
-                print(f"Enviando PUT_OK ao Cliente {ip}]:{client_address[1]} da key:{key} ts:[timestamp_do_servidor]")
+                print(f"Enviando PUT_OK ao Cliente {ip}]:{client_address[1]} da key:{key} ts:[{mensagem.timestamp}]")
                 client_socket.send(str(timestamp).encode())
             else:
-                print("aqui")
+                # Direcionar o PUT ao lider
                 lider_connect_socket.send("PUT".encode())
+                lider_connect_socket.send(str(client_address[1]).encode('utf-8'))
+                # Receber a mensagem (json) do servidor
                 json_mensagem = client_socket.recv(1024)
-                lider_connect_socket.send(json_mensagem)
-                # Deserialize the JSON data to reconstruct the Mensagem object
+                # deserializar json e montar tabela chave-valor
                 mensagem_dict = json.loads(json_mensagem)
                 key = mensagem_dict["key"]
                 value = mensagem_dict["value"]
-                # devolver para o cliente o PUT_OK com timestamp
-                print(f"Enviando PUT_OK ao Cliente {ip}]:{client_address[1]} da key:{key} ts:[timestamp_do_servidor]")
+                lider_connect_socket.send(json_mensagem)
+                print(f"Encaminhando PUT key:{key} value: {value}.")
+                # devolver para o cliente o timestamp
                 client_socket.send(str(timestamp).encode())
+        elif operacao == "GET":
+            key = client_socket.recv(1024).decode()
+            for k, v in key_value.items():
+                if k == key:
+                    value, timestamp = v
+                    mensagem = Mensagem(key, value, timestamp)
+                    mensagem_get = json.dumps({"key": mensagem.key,
+                                               "value": mensagem.value,
+                                               "timestamp": mensagem.timestamp})
+                    # Devolver a mensagem ao cliente
+                    client_socket.send(mensagem_get.encode('utf-8'))
+                    # Enviar o timestamp do servidor
+                    client_socket.send(str(mensagem.timestamp).encode('utf-8'))
+
+                    print(f"Cliente [{ip}]:[{client_address[1]}] GET key:[{mensagem.key}] ts:[{mensagem.timestamp}]. "
+                          f"Meu ts é {mensagem.timestamp}], portanto"
+                          f"devolvendo [{mensagem.value}].")
+                    break
+            # Caso nao encontre a chave, devolva NULL
+            if key not in key_value:
+                client_socket.send("NULL".encode('utf-8'))
 
 
 def server_put_thread(server_socket):
     while True:
         data = server_socket.recv(1024).decode('utf-8')
         if data == "PUT":
+            # receber porta do cliente que fez a requisicao
+            client_address = server_socket.recv(1024).decode('utf-8')
             timestamp = time.time()
-            # Receive JSON data from the server
+            # Receber a mensagem (json) do servidor
             json_mensagem = server_socket.recv(1024).decode('utf-8')
-            # Deserialize the JSON data to reconstruct the Mensagem object
+            # deserializar json e montar tabela chave-valor
             mensagem_dict = json.loads(json_mensagem)
             key_value[mensagem_dict["key"]] = (mensagem_dict["value"], timestamp)
-            print(key_value)
-
             # replicar a chave-valor nos servidores conectados ao lider
             mensagem = Mensagem(mensagem_dict["key"], mensagem_dict["value"], timestamp)
             mensagem_replication = json.dumps({"key": mensagem.key,
                                                "value": mensagem.value,
                                                "timestamp": mensagem.timestamp})
+            key = mensagem_dict["key"]
             for i in range(len(replication_socket_list)):
                 replication_socket_list[i].send("REPLICATION".encode('utf-8'))
                 replication_socket_list[i].send(mensagem_replication.encode('utf-8'))
                 # receber a confirmação da replicação
                 replication_socket_list[i].recv(1024).decode()
+            # devolver para o cliente o PUT_OK com timestamp
+            print(f"Enviando PUT_OK ao Cliente {ip}]:{client_address} da key:{key} ts:[timestamp_do_servidor]")
 
 
 def server_replication_thread(replication_socket):
@@ -115,7 +132,6 @@ def server_replication_thread(replication_socket):
             key = mensagem_dict["key"]
             value = mensagem_dict["value"]
             timestamp = mensagem_dict["timestamp"]
-            print(key_value)
             print(f"REPLICATION key:{key} value:{value} ts:{timestamp}.")
             replication_socket.send("REPLICATION_OK".encode())
 
@@ -156,30 +172,20 @@ def main():
         server_replication = threading.Thread(target=server_replication_thread, args=(replication_socket,))
         server_replication.start()
 
-    # envio de mensagem do lider para os servidores
-    if lider:
-        for i in range(2):
-            servers_socket_list[i].send("estamos conectados".encode())
-    else:
-        data = lider_connect_socket.recv(1024).decode('utf-8')
-        print(data)
-
     try:
         server_client_socket.bind((ip, porta_input))
         server_client_socket.listen(4)
 
         while True:
-            # Accept a client connection
+            # Aceitar conexão com cliente
             client_socket, client_address = server_client_socket.accept()
-
-            # Create a new thread to handle the client connection
+            # Criar thread que processa as requisições dos clientes
             client_thread = threading.Thread(target=handle_client, args=(client_socket,
                                                                          client_address,
                                                                          lider_connect_socket))
             client_thread.start()
 
     except KeyboardInterrupt:
-        # Close the server socket when the user interrupts the program
         server_client_socket.close()
 
 
